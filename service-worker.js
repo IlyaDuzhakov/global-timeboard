9b49127e7a6ac78cd44220cd6bdb31264b22b392
IlyaDuzhakov <IlyaDuzhakov@users.noreply.github.com>
2026-05-28 19:11:24 +0000

    deploy: ab56cd4faf20ce25fa7ec98369f1fb9833dce964

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-restricted-globals */
+
+const CACHE_NAME = "global-timeboard-v1";
+
+const urlsToCache = [
+  "/global-timeboard/",
+  "/global-timeboard/index.html",
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+
+          return null;
+        }),
+      ),
+    ),
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
