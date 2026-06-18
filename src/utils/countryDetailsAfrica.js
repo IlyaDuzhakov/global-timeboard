69229ef847ae1220ba1a04ab9963ad6610e2327d
IlyaDuzhakov <duzhakovip@mail.ru>
2026-06-18 22:19:03 +0400

    add countries

---
+++ src/utils/countryDetailsAfrica.js
@@ -0,0 +1,13 @@
+import { northAfrica } from "./countries/africa/northAfrica.js";
+import { westAfrica } from "./countries/africa/westAfrica.js";
+import { centralAfrica } from "./countries/africa/centralAfrica.js";
+import { eastAfrica } from "./countries/africa/eastAfrica.js";
+import { southernAfrica } from "./countries/africa/southernAfrica.js";
+
+export const countryDetailsAfrica = {
+  ...northAfrica,
+  ...westAfrica,
+  ...centralAfrica,
+  ...eastAfrica,
+  ...southernAfrica,
+};
